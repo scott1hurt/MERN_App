a759f845b65b4e69aaafbc3f38dba1f8f60cb5cb
Scott Hurt <scotthurt@MacBook-Pro-55.local>
2019-11-25 10:14:35 -0600

    Added in gitignore to ignore node modules, installed packages for server'

--- backend/secret.js
+++
@@ -1,8 +0,0 @@
-const secrets = { 
-    //The URL that we use to connect to the MongoDB Atlas Cluster
-    dbUrl: 'mongodb+srv://Scott1hurt:usahungary50@cluster0-ja6jg.mongodb.net/test?retryWrites=true&w=majority'
-};
-
-const getSecret = key => secrets[key];
-
-module.exports = getSecret;
--- client/server.js
+++ client/server.js
@@ -1,3 +1,4 @@
 const mongoose = require ('mongoose');
 const express = require ('express');
 const logger = require ('morgan');
+const bodyParser = require ('body-parser');
